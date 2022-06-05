c3da044fecc25d37ad331e14ab6386c74ebdb309
Tomy Suarez <tomy.suarez@estudiantes.utec.edu.uy>
2022-06-05 18:28:48 -0300

    fasf

--- conjunto/Gestor.py
+++ conjunto/Gestor.py
@@ -5,29 +5,10 @@ def plan_estudio():
     return df.fillna("No tiene")
 
 class Coordinador():
-    def __init__(self, cedula, nombreuc=None,nombreEx=None):
-        if self.existencia(self, cedula)==1:
-            self.nombreuc= nombreuc
-            self.nombreEx= nombreEx
-            print("Bienvenido")
-        elif self.existencia(self, cedula)==2:
-            raise ValueError("Usted no existe en la base de datos")
-        else:
-            raise ValueError("Usted no es un coordinador/a")
-    
-    def existencia(self, cedula):
-        with open('rol.csv') as File:
-            reader = csv.DictReader(File)
-            for row in reader:
-                for keys, values in row.items():
-                    if values == str(cedula) in values:
-                        results=list(row.values())
-                        if results[2]=="Coordinador/a":
-                            return 1
-                        else:
-                            return 0
-                    else:
-                        return 2
+    def __init__(self, nombreuc=None,nombreEx=None):
+        self.nombreuc= nombreuc
+        self.nombreEx= nombreEx
+
     def leer_uc(self):
         return pd.read_csv(f"matriculadas/disponibles/{self.nombreuc}.csv ") 
         
@@ -37,31 +18,12 @@ class Coordinador():
 
 class Estudiante():
     def __init__(self, cedula,matricEX=None, matricUC=None):
-        if self.existencia(self, cedula)==1:
-            self.matricUC= matricUC
-            self.matricEx= matricEX
-            self.ci= cedula
-            print("Bienvenido")
-        elif self.existencia(self, cedula)==2:
-            raise ValueError("Usted no existe en la base de datos")
-        else:
-            raise ValueError("Usted no es Estudiante")
-    
-    def existencia(self, cedula):
-        with open('rol.csv') as File:
-            reader = csv.DictReader(File)
-            for row in reader:
-                for keys, values in row.items():
-                    if values == str(cedula) in values:
-                        results=list(row.values())
-                        if results[2]=="Estudiante":
-                            return 1
-                        else:
-                            return 0
-                    else:
-                        return 2
+        self.matricUC= matricUC
+        self.matricEx= matricEX
+        self.ci= cedula
+       
     def matricularEx(self):
-        with open(f"examenes/disponibles/{self.matricEx}.csv",'a', newline='') as File:
+        with open(f"examenes/disponibles/{nuevoss}.csv",'a', newline='') as File:
             leer1= csv.writer(File)
             with open(f'rol.csv', 'r', newline='') as nuevo:
                 leer= csv.DictReader(nuevo)
@@ -69,41 +31,23 @@ class Estudiante():
                     for key, value in i.items():
                         if value == "54311063":
                             estudiante= [i["Nombres"],i["CI"]]
-                            leer1.writerow(estudiante)
-                            return f"estudiante {estudiante[0]} matriculado"
-                nuevo.close()
-            File.close()
+            leer1.writerow(estudiante)
+            nuevo.close()
+        File.close()
+        return estudiante
+
         
     def matricularUC(self):
         pass
 
 class Administrador():
     def __init__(self, cedula,nombrEst=None,matricUC=None, matricEX=None, DesmUC=None, DesmEX=None, nombreuc=None,nombreEx=None ):
-        if self.existencia(self, cedula)==1:
-            self.matricUC= matricUC
-            self.matricEx= matricEX
-            self.uc= nombreuc
-            self.ex= nombreEx
-            self.est= nombrEst
-            print("Bienvenido")
-        elif self.existencia(self, cedula)==2:
-            raise ValueError("Usted no existe en la base de datos")
-        else:
-            raise ValueError("Usted no es Administrativa/o")
+        self.matricUC= matricUC
+        self.matricEx= matricEX
+        self.uc= nombreuc
+        self.ex= nombreEx
+        self.est= nombrEst
     
-    def existencia(self, cedula):
-        with open('rol.csv') as File:
-            reader = csv.DictReader(File)
-            for row in reader:
-                for keys, values in row.items():
-                    if values == str(cedula) in values:
-                        results=list(row.values())
-                        if results[2]=="Administrativa/o":
-                            return 1
-                        else:
-                            return 0
-                    else:
-                        return 2
     def matricularEx(self):
         pass
     def desmatricularEx(self):
@@ -118,3 +62,7 @@ class Administrador():
         
     def leer_Ex(self):
         return pd.read_csv(f"examenes/disponibles/{self.ex}.csv ")
+
+
+matricular=Estudiante(54311063,"S6UC2")
+print(matricular.matricularEx())
---
+++ conjunto/pruebas.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from Gestor import Estudiante,Coordinador, Administrador
+matricular=Estudiante(324435235,"S6UC2")
+matricular.matricularEx()
